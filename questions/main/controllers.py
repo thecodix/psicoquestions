3dd4d7aef693790a7586cf7995049d9417944381
Alfredo Romeu <romeu.alfred@gmail.com>
2020-03-22 17:22:04 +0100

    Add questions route

--- questions/main/controllers.py
+++ questions/main/controllers.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-
+from flask import render_template
 
 main = Blueprint('main', __name__)
 
@@ -7,3 +7,17 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return "Main"
+
+@main.route('questions/')
+def display_questions():
+    questions = [
+        {
+            "title": "Question 1?",
+            "answers": [
+                "True",
+                "False",
+            ]
+        }
+    ]
+
+    return render_template("questions.htm", questions=questions)

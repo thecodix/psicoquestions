2dcaa98b07c1010f49e9e37258320ad2e586b70c
Alfredo Romeu <romeu.alfred@gmail.com>
2020-03-22 17:06:03 +0100

    configuration

--- questions/__init__.py
+++ questions/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from questions.main.controllers import main
+
 from questions.admin.controllers import admin
+from questions.config import configure_app
+from questions.main.controllers import main
 
 app = Flask(__name__)
+
+configure_app(app)
 
 app.register_blueprint(main, url_prefix='/')
 app.register_blueprint(admin, url_prefix='/admin')
---
+++ questions/config.py
@@ -0,0 +1,29 @@
+import os
+
+
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = False
+    TESTING = True
+
+
+config = {
+    "dev": "questions.config.DevelopmentConfig",
+    "testing": "questions.config.TestingConfig",
+    "default": "questions.config.DevelopmentConfig",
+}
+
+
+def configure_app(app):
+    config_name = os.getenv("FLASK_CONFIGURATION", "default")
+    app.config.from_object(config[config_name])
+    #TODO load configuration from file
